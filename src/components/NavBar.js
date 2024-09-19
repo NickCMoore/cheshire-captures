1dd704e57f81af1ba54dec41bfcd0e7471e2a040
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-09-20 00:15:33 +0100

    feat: integrate react-router-dom for navigation links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import styles from './NavBar.module.css';
 
 function NavBar() {
   return (
     <Navbar bg="light" expand="lg" className={styles.NavBar}>
-      <Navbar.Brand href="/">Cheshire Captures</Navbar.Brand>
+      <Navbar.Brand as={NavLink} to="/">Cheshire Captures</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/gallery">Gallery</Nav.Link>
+          <Nav.Link as={NavLink} to="/" exact>Home</Nav.Link>
+          <Nav.Link as={NavLink} to="/gallery">Gallery</Nav.Link>
           <NavDropdown title="More" id="basic-nav-dropdown">
-            <NavDropdown.Item href="/about">About</NavDropdown.Item>
-            <NavDropdown.Item href="/contact">Contact</NavDropdown.Item>
+            <NavDropdown.Item as={NavLink} to="/about">About</NavDropdown.Item>
+            <NavDropdown.Item as={NavLink} to="/contact">Contact</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+            <NavDropdown.Item as={NavLink} to="/signin">Sign in</NavDropdown.Item>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
@@ -24,5 +25,4 @@ function NavBar() {
 }
 
 export default NavBar;
-
 
