52d5c569bed6da29a501bc4f6dc1cf043fb88731
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-09-19 23:37:41 +0100

    feat: add routes and tidy navbar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter from react-router-dom
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter> {/* Wrap your App with BrowserRouter */}
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
