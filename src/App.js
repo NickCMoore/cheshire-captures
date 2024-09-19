4e5f48588a0f5c59dfceb55eb0341d719fe3b487
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-09-19 23:55:29 +0100

    fix: audit decprecated packages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
@@ -12,10 +12,9 @@ function App() {
           <Route exact path="/" render={() => <h1>Welcome to Cheshire Captures</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
           <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
-          <Route render={() => <p>Page not found!</p>}/>
+          <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
-      <h1>Welcome to Cheshire Captures</h1>
     </div>
   );
 }
