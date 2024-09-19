0ed3e6e5b110482f8d5557e5c7005e5dc41ff6b0
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-09-19 23:20:30 +0100

    fix: wrap app component with browswerrouter

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <h1>Welcome to Cheshire Captures</h1>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,42 +1,22 @@
 import React from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import styles from './NavBar.module.css';
 
 function NavBar() {
   return (
     <Navbar bg="light" expand="lg" className={styles.NavBar}>
-      <Navbar.Brand as={Link} to="/">
-        Cheshire Captures
-      </Navbar.Brand>
+      <Navbar.Brand href="/">Cheshire Captures</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link as={Link} to="/">
-            Home
-          </Nav.Link>
-          <Nav.Link as={Link} to="/about">
-            About
-          </Nav.Link>
-          {/* Add your NavDropdown here */}
-          <NavDropdown title="Categories" id="basic-nav-dropdown">
-            <NavDropdown.Item as={Link} to="/category/nature">
-              Nature
-            </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="/category/landscape">
-              Landscape
-            </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="/category/cityscape">
-              Cityscape
-            </NavDropdown.Item>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/gallery">Gallery</Nav.Link>
+          <NavDropdown title="More" id="basic-nav-dropdown">
+            <NavDropdown.Item href="/about">About</NavDropdown.Item>
+            <NavDropdown.Item href="/contact">Contact</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item as={Link} to="/category/other">
-              Other
-            </NavDropdown.Item>
+            <NavDropdown.Item href="/login">Login</NavDropdown.Item>
           </NavDropdown>
-          <Nav.Link as={Link} to="/contact">
-            Contact
-          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
@@ -44,4 +24,5 @@ function NavBar() {
 }
 
 export default NavBar;
+
 
