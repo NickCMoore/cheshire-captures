527f34c57eb9161e961795c50aa73e982fba9e49
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-09-22 21:49:09 +0100

    style: refactor signin/up pages and align content

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -2,55 +2,50 @@ import React from 'react';
 import { Form, Button, Col, Row, Container } from 'react-bootstrap';
 import styles from '../../styles/SignInUpForm.module.css';
 import btnStyles from '../../styles/Button.module.css';
-import appStyles from '../../App.module.css';
 import { Link } from 'react-router-dom';
 
 const SignInForm = () => {
   return (
-    <Row className={styles.Row}>
-      <Col className="my-auto py-2 p-md-2" md={6}>
-        <Container className={`${appStyles.Content} p-4`}>
-          <h1 className={styles.Header}>Sign in</h1>
-          <Form>
-            <Form.Group controlId="email">
-              <Form.Label className="d-none">Email</Form.Label>
-              <Form.Control
-                className={styles.Input}
-                type="email"
-                placeholder="Enter email"
-                name="email"
-              />
-            </Form.Group>
-            <Form.Group controlId="password">
-              <Form.Label className="d-none">Password</Form.Label>
-              <Form.Control
-                className={styles.Input}
-                type="password"
-                placeholder="Password"
-                name="password"
-              />
-            </Form.Group>
-            <Button
-              className={`${btnStyles.Button} ${btnStyles.Bright} ${btnStyles.Wide}`}
-              type="submit"
-            >
-              Sign in
-            </Button>
-          </Form>
-        </Container>
-        <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/signup">
-            Don't have an account? <span>Sign up</span>
-          </Link>
-        </Container>
-      </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
-      >
-        {/* Img to be added */}
-      </Col>
-    </Row>
+    <Container fluid className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh', backgroundColor: '#f1f1f1' }}>
+      <Row className="w-100 justify-content-center">
+        <Col xs={12} md={6} lg={4} className="mx-auto">
+          <div className={styles.FormContainer}>
+            <h1 className={styles.Header}>Sign in</h1>
+            <Form>
+              <Form.Group controlId="email">
+                <Form.Label className="d-none">Email</Form.Label>
+                <Form.Control
+                  className={styles.Input}
+                  type="email"
+                  placeholder="Enter email"
+                  name="email"
+                />
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label className="d-none">Password</Form.Label>
+                <Form.Control
+                  className={styles.Input}
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                />
+              </Form.Group>
+              <Button
+                className={`${btnStyles.Button} ${btnStyles.Bright} ${btnStyles.Wide}`}
+                type="submit"
+              >
+                Sign in
+              </Button>
+            </Form>
+            <Container className="mt-3">
+              <Link className={styles.Link} to="/signup">
+                Don't have an account? <span>Sign up</span>
+              </Link>
+            </Container>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,72 +1,61 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styles from "../../styles/SignInUpForm.module.css";
-import btnStyles from "../../styles/Button.module.css";
-import appStyles from "../../App.module.css";
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Image from "react-bootstrap/Image";
+import React from 'react';
+import { Form, Button, Col, Row, Container } from 'react-bootstrap';
+import styles from '../../styles/SignInUpForm.module.css';
+import btnStyles from '../../styles/Button.module.css';
+import { Link } from 'react-router-dom';
 
 const SignUpForm = () => {
   return (
-    <Row className={styles.Row}>
-      <Col className="my-auto py-2 p-md-2" md={6}>
-        <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign up</h1>
-          <Form>
-            <Form.Group controlId="username">
-              <Form.Label className="d-none">Username</Form.Label>
-              <Form.Control 
-                className={styles.Input}
-                type="text" 
-                placeholder="Enter username" 
-                name="username"
-            />
-            </Form.Group>
-            <Form.Group controlId="password1">
-              <Form.Label className="d-none">Password</Form.Label>
-              <Form.Control 
-                className={styles.Input}
-                type="password" 
-                placeholder="Password"
-                name="password1"
-              />
-            </Form.Group>
-            <Form.Group controlId="password2">
-              <Form.Label className="d-none">Confirm password</Form.Label>
-              <Form.Control 
-                className={styles.Input}
-                type="password" 
-                placeholder="Confirm password"
-                name="password2"
-              />
-            </Form.Group>
-            <Button className={`${btnStyles.Button} ${btnStyles.Bright} ${btnStyles.Wide}`} type="submit">
-              Sign up
-            </Button>
-          </Form>
-        </Container>
-        <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/signin">
-            Already have an account? <span>Sign in</span>
-          </Link>
-        </Container>
-      </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
-      >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          src={
-            ""
-          }
-        />
-      </Col>
-    </Row>
+    <Container fluid className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh', backgroundColor: '#f1f1f1' }}>
+      <Row className="w-100 justify-content-center">
+        <Col xs={12} md={6} lg={4} className="mx-auto">
+          <div className={styles.FormContainer}>
+            <h1 className={styles.Header}>Sign up</h1>
+            <Form>
+              <Form.Group controlId="username">
+                <Form.Label className="d-none">Username</Form.Label>
+                <Form.Control
+                  className={styles.Input}
+                  type="text"
+                  placeholder="Enter username"
+                  name="username"
+                />
+              </Form.Group>
+              <Form.Group controlId="password1">
+                <Form.Label className="d-none">Password</Form.Label>
+                <Form.Control
+                  className={styles.Input}
+                  type="password"
+                  placeholder="Password"
+                  name="password1"
+                />
+              </Form.Group>
+              <Form.Group controlId="password2">
+                <Form.Label className="d-none">Confirm password</Form.Label>
+                <Form.Control
+                  className={styles.Input}
+                  type="password"
+                  placeholder="Confirm password"
+                  name="password2"
+                />
+              </Form.Group>
+              <Button
+                className={`${btnStyles.Button} ${btnStyles.Bright} ${btnStyles.Wide}`}
+                type="submit"
+              >
+                Sign up
+              </Button>
+            </Form>
+            <Container className="mt-3">
+              <Link className={styles.Link} to="/signin">
+                Already have an account? <span>Sign in</span>
+              </Link>
+            </Container>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
+
 export default SignUpForm;
