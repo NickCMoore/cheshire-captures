c492d5ea82da30a279dc46844a8cbf901f7c0b32
Nick Moore <moorenickc@protonmail.com>
2024-10-01 13:53:16 +0000

    fix: Fix navigation links in NavBar to use NavLink for routing to Sign In and Sign Up pages #1 #2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import styles from '../styles/NavBar.module.css';
 import { NavLink } from 'react-router-dom';
+import styles from '../styles/NavBar.module.css';
 import logo from '../assets/Screenshot 2024-09-23 095558.png'
 
 const NavBar = () => {
@@ -8,21 +8,25 @@ const NavBar = () => {
     <Navbar bg="-grey" expand="md" fixed="top" className={styles.NavBar}>
       <Container>
         <NavLink to="/">
-            <Navbar.Brand>
-                <img src={logo} alt="logo" height="50"/>
-            </Navbar.Brand>
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="50" />
+          </Navbar.Brand>
         </NavLink>
         <Navbar.Toggle aria-controls="navbar-collapse" />
         <Navbar.Collapse id="navbar-collapse">
           <Nav className={`${styles.Nav} ml-auto`}>
-            <Nav.Link>
-              <i className='fas fa-house'></i>Home</Nav.Link>
-            <Nav.Link>
-              <i class="fa-solid fa-panorama"></i>Gallery</Nav.Link>
-            <Nav.Link>
-              <i class="fa-solid fa-user-plus"></i>Sign in</Nav.Link>
-            <Nav.Link>
-              <i class="fa-solid fa-arrow-right-to-bracket"></i>Sign up</Nav.Link>
+            <NavLink exact to="/" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className='fas fa-house'></i> Home
+            </NavLink>
+            <NavLink exact to="/gallery" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fa-solid fa-panorama"></i> Gallery
+            </NavLink>
+            <NavLink exact to="/signin" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fa-solid fa-user-plus"></i> Sign In
+            </NavLink>
+            <NavLink exact to="/signup" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fa-solid fa-arrow-right-to-bracket"></i> Sign Up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
