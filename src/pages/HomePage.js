b4e8b0421ef307bba587154d66b47e47833a28b5
Nick Moore <moorenickc@protonmail.com>
2024-10-02 11:15:33 +0000

    chore: update dependencies

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,7 +8,7 @@ const HomePage = () => {
       <div className={styles.FullScreenOverlay}></div>
       <div className={styles.FullScreenContent}>
         <h1>Welcome to Cheshire Captures</h1>
-        <p>Capture and share your favorite moments with the community.</p>
+        <p>Capture and share your favourite moments with the community.</p>
         <Link to="/signup">
           <button className={styles.Button}>Get Started</button>
         </Link>
