5e6eed9e847af7b7f23d8d8bf47830c96de6927c
Nick Moore <moorenickc@protonmail.com>
2024-10-04 16:16:23 +0000

    chore: update axios defaults to backendurl

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://cheshire-captures-4a500dc7ab0a.herokuapp.com/";
+axios.defaults.baseURL = "https://cheshire-captures-backend-084aac6d9023.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
