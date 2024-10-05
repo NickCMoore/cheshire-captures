3cfdd00515898f6ab8b5bb58f251475095217bfa
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-10-05 14:53:36 +0100

    refactor: tidy up app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container'
 import Profile from './pages/Profile'; 
 import HomePage from './pages/HomePage';
 import SignInForm from './pages/auth/SignInForm';
 import SignUpForm from './pages/auth/SignUpForm';
 import Gallery from './pages/Gallery';
 import AboutPage from './pages/AboutPage';
-import { AuthProvider } from './contexts/AuthContext'; 
 import './api/axiosDefaults';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './App.module.css';
@@ -15,17 +15,17 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.App}>
-      <AuthProvider>
           <NavBar />
-          <Switch>
+          <Container className={styles.Main}>
+            <Switch>
             <Route exact path="/" component={HomePage} />
             <Route exact path="/signin" component={SignInForm} />
             <Route exact path="/signup" component={SignUpForm} />
             <Route exact path="/gallery" component={Gallery} />
             <Route exact path="/about" component={AboutPage} />
             <Route exact path="/profile/:id" component={Profile} />
-          </Switch>
-      </AuthProvider>
+            </Switch>
+          </Container>
     </div>
   );
 }
