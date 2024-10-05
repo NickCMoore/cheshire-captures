473e012b907e76427421996651edd431e5420c15
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-10-05 14:37:22 +0100

    refactor: tidy up NavBar for home screen

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,39 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext'; 
 import styles from '../styles/NavBar.module.css';
 import logo from '../assets/cc-logo.png';
 
 const NavBar = () => {
-  const [expanded, setExpanded] = useState(false);
-  const { currentUser, logoutUser, loading } = useAuth(); 
-
-  const handleLogout = async () => {
-    try {
-      await logoutUser();
-      setExpanded(false); 
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-
-  if (loading) {
-    return null; 
-  }
-
   return (
-    <Navbar bg="-grey" expand="md" fixed="top" className={styles.NavBar} expanded={expanded}>
+    <Navbar expand="md" fixed="top" className={styles.NavBar}>
       <Container>
-        <NavLink to="/" onClick={() => setExpanded(false)}>
+        <NavLink to="/">
           <Navbar.Brand>
-            <img src={logo} alt="logo" height="60" />
+            <img src={logo} alt="logo" height="50" />
           </Navbar.Brand>
         </NavLink>
-        <Navbar.Toggle aria-controls="navbar-collapse" onClick={() => setExpanded(expanded ? false : "expanded")} />
+        <Navbar.Toggle aria-controls="navbar-collapse" />
         <Navbar.Collapse id="navbar-collapse" className={styles.NavbarCollapse}>
-          <Nav className={`${styles.Nav} ml-auto`} onSelect={() => setExpanded(false)}>
+          <Nav className={`${styles.Nav} ml-auto`} >
             <NavLink exact to="/" className={styles.NavLink} activeClassName={styles.Active}>
               <i className='fas fa-house'></i> Home
             </NavLink>
@@ -43,26 +25,12 @@ const NavBar = () => {
             <NavLink exact to="/about" className={styles.NavLink} activeClassName={styles.Active}>
               <i className="fa-solid fa-info-circle"></i> About
             </NavLink>
-
-            {currentUser ? (
-              <>
-                <NavLink exact to={`/profile/${currentUser.username}`} className={styles.NavLink} activeClassName={styles.Active}>
-                  <i className="fa-solid fa-user"></i> Profile
-                </NavLink>
-                <NavLink exact to="/" onClick={handleLogout} className={styles.NavLink}>
-                  <i className="fa-solid fa-sign-out-alt"></i> Sign Out
-                </NavLink>
-              </>
-            ) : (
-              <>
-                <NavLink exact to="/signin" className={styles.NavLink} activeClassName={styles.Active}>
-                  <i className="fa-solid fa-sign-in-alt"></i> Sign In
-                </NavLink>
-                <NavLink exact to="/signup" className={styles.NavLink} activeClassName={styles.Active}>
-                  <i className="fa-solid fa-user-plus"></i> Sign Up
-                </NavLink>
-              </>
-            )}
+            <NavLink exact to="/signin" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fa-solid fa-sign-in-alt"></i> Sign In
+            </NavLink>
+            <NavLink exact to="/signup" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fa-solid fa-user-plus"></i> Sign Up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
