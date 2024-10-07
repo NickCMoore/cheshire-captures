7e5f250077c8864bb4fc93c2c157708fac4ae010
Nick Moore <moorenickc@protonmail.com>
2024-10-07 19:45:05 +0000

    refactor axios defaults

--- src/App.js
+++ src/App.js
@@ -1,51 +1,65 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import Container from 'react-bootstrap/Container';
-import Profile from './pages/Profile'; 
-import HomePage from './pages/HomePage';
-import SignInForm from './pages/auth/SignInForm';
-import SignUpForm from './pages/auth/SignUpForm';
-import { useCurrentUser } from './contexts/CurrentUserContext';
-import Gallery from './pages/Gallery';
-import AboutPage from './pages/AboutPage';
-import './api/axiosDefaults';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from './App.module.css';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./api/axiosDefaults";
+import HomePage from "./pages/HomePage";
+import SignInForm from "./pages/auth/SignInForm";
+import SignUpForm from "./pages/auth/SignUpForm";
+import Gallery from "./pages/Gallery";
+import AboutPage from "./pages/AboutPage";
+import Profile from "./pages/Profile";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 
 function App() {
-
   const currentUser = useCurrentUser();
 
-
   return (
-        <div className={styles.App}>
-          <NavBar />
-          <Container className={styles.Main}>
-            <Switch>
-              <Route exact path="/" render={() => <HomePage />} />
-              <Route 
-                exact 
-                path="/signin" 
-                render={() => currentUser ? <Redirect to="/" /> : <SignInForm />} 
-              />
-              <Route 
-                exact 
-                path="/signup" 
-                render={() => currentUser ? <Redirect to="/" /> : <SignUpForm />} 
-              />
-              <Route exact path="/gallery" render={() => <Gallery />} />
-              <Route exact path="/about" render={() => <AboutPage />} />
-              <Route 
-                exact 
-                path="/profile/:id" 
-                render={() => currentUser ? <Profile /> : <Redirect to="/signin" />} 
-              />
-            </Switch>
-          </Container>
-        </div>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => <HomePage />}
+          />
+          <Route
+            exact
+            path="/gallery"
+            render={() => <Gallery />}
+          />
+          <Route
+            exact
+            path="/about"
+            render={() => <AboutPage />}
+          />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              currentUser ? <Redirect to="/" /> : <SignInForm />
+            }
+          />
+          <Route
+            exact
+            path="/signup"
+            render={() =>
+              currentUser ? <Redirect to="/" /> : <SignUpForm />
+            }
+          />
+          <Route
+            exact
+            path="/profile/:id"
+            render={() =>
+              currentUser ? <Profile /> : <Redirect to="/signin" />
+            }
+          />
+        </Switch>
+      </Container>
+    </div>
   );
 }
 
 export default App;
-
--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,60 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://cheshire-captures-backend-084aac6d9023.herokuapp.com";
-axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.baseURL = "https://cheshire-captures-backend-084aac6d9023.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
-export const axiosReq = axios.create({
-  baseURL: axios.defaults.baseURL,
-  withCredentials: true,
-});
-
-export const axiosRes = axios.create({
-  baseURL: axios.defaults.baseURL,
-  withCredentials: true,
-});
-
-axiosReq.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("access_token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-axiosRes.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-
-      try {
-        const refreshToken = localStorage.getItem("refresh_token");
-        if (refreshToken) {
-          const { data } = await axios.post("/dj-rest-auth/token/refresh/", {
-            refresh: refreshToken,
-          });
-
-          localStorage.setItem("access_token", data.access);
-
-          axiosReq.defaults.headers.Authorization = `Bearer ${data.access}`;
-          originalRequest.headers.Authorization = `Bearer ${data.access}`;
-
-          return axiosReq(originalRequest);
-        }
-      } catch (refreshError) {
-        localStorage.removeItem("access_token");
-        localStorage.removeItem("refresh_token");
-        window.location.href = "/login";
-        return Promise.reject(refreshError);
-      }
-    }
-
-    return Promise.reject(error);
-  }
-);
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
