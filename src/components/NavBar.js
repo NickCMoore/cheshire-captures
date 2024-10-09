6b414819fa4d54cf0c06d88069dcd7f65bc1b4b8
Nick Moore <moorenickc@protonmail.com>
2024-10-09 09:37:20 +0000

    feat: update NavBar links to better reflect user status

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,28 +10,38 @@ import styles from '../styles/NavBar.module.css';
 import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
 import { removeTokenTimestamp } from '../utils/Utils';
 // React imports
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation, useHistory } from 'react-router-dom';
 import { Link } from "react-router-dom";
 import { useState } from 'react';
 // Axios imports
 import axios from 'axios';
 
 const NavBar = () => {
-
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+  const location = useLocation();
+  const history = useHistory();
 
-  // Variables to toggle open and closed mobile navbar burger menu
   const [toggleNavBar, setToggleNavBar] = useState(false);
 
   const handleSignOut = async () => {
     try {
-      await axios.post("dj-rest-auth/logout/");
+      await axios.post('dj-rest-auth/logout/');
       setCurrentUser(null);
       setToggleNavBar(!toggleNavBar);
       removeTokenTimestamp();
+      history.push('/'); 
     } catch (err) {
       console.log(err);
+    }
+  };
+
+  const isOnOwnProfile = currentUser && location.pathname === `/profiles/${currentUser.photographer_id}`;
+
+  const handleProfileClick = () => {
+    if (currentUser) {
+      history.push(`/profiles/${currentUser.photographer_id}`);
+      setToggleNavBar(false); 
     }
   };
 
@@ -42,9 +52,7 @@ const NavBar = () => {
         className={styles.NavLink}
         activeClassName={styles.Active}
         to="/gallery"
-        onClick={() => {
-          setToggleNavBar(!toggleNavBar);
-        }}
+        onClick={() => setToggleNavBar(!toggleNavBar)}
       >
         <i className="fas fa-images"></i>Gallery
       </NavLink>
@@ -53,48 +61,31 @@ const NavBar = () => {
         className={styles.NavLink}
         activeClassName={styles.Active}
         to="/popular-photographers"
-        onClick={() => {
-          setToggleNavBar(!toggleNavBar);
-        }}
+        onClick={() => setToggleNavBar(!toggleNavBar)}
       >
         <i className="fas fa-users"></i>Popular Photographers
       </NavLink>
+      {!isOnOwnProfile && (
+        <NavLink
+          exact
+          className={styles.NavLink}
+          activeClassName={styles.Active}
+          to={`/profiles/${currentUser?.photographer_id}`}
+          onClick={handleProfileClick}
+        >
+          <i className="fas fa-user"></i>Profile
+        </NavLink>
+      )}
       <NavLink
         exact
         className={styles.NavLink}
-        activeClassName={styles.Active}
-        to={`/profiles/${currentUser?.photographer_id}`}
-        onClick={() => {
-          setToggleNavBar(!toggleNavBar);
-        }}
-      >
-        <i className="fas fa-user"></i>Profile
-      </NavLink>
-    </>
-  );
-
-  const loggedInDropdownIcons = (
-    <>
-      <NavDropdown.Item 
-        id={styles.dropdownItem}
-        as={Link} 
-        to={`/profiles/${currentUser?.photographer_id}`}
-        onClick={() => {
-          setToggleNavBar(!toggleNavBar);
-        }}
-      >
-        <i className="fas fa-user-circle"></i>Profile
-      </NavDropdown.Item>
-      <NavDropdown.Item 
-        id={styles.dropdownItem}
-        as={Link} 
-        to="/" 
+        to="/"
         onClick={handleSignOut}
       >
         <i className="fas fa-sign-out-alt"></i>Sign out
-      </NavDropdown.Item>
+      </NavLink>
     </>
-  )
+  );
 
   const loggedOutIcons = (
     <>
@@ -102,49 +93,32 @@ const NavBar = () => {
         exact
         className={styles.NavLink}
         activeClassName={styles.Active}
-        to="/popular-photographers"
-        onClick={() => {
-          setToggleNavBar(!toggleNavBar);
-        }}
-      >
-        <i className="fas fa-users"></i>Popular Photographers
-      </NavLink>
-      <NavLink
-        exact
-        className={styles.NavLink}
-        activeClassName={styles.Active}
         to="/about"
-        onClick={() => {
-          setToggleNavBar(!toggleNavBar);
-        }}
+        onClick={() => setToggleNavBar(!toggleNavBar)}
       >
         <i className="fas fa-info-circle"></i>About
       </NavLink>
-    </>
-  );
-
-  const loggedOutDropdownIcons = (
-    <>
-      <NavDropdown.Item 
-        id={styles.dropdownItem}
-        as={Link} 
-        to="/signin"
-        onClick={() => {
-          setToggleNavBar(!toggleNavBar);
-        }}
+      <NavDropdown 
+        title={<span><i className="fas fa-user-alt ml-5"></i></span>}
+        id="basic-nav-dropdown"
       >
-        <i className="fas fa-sign-in-alt"></i>Sign in
-      </NavDropdown.Item>
-      <NavDropdown.Item 
-        id={styles.dropdownItem}
-        as={Link} 
-        to="/signup"
-        onClick={() => {
-          setToggleNavBar(!toggleNavBar);
-        }}
-      >
-        <i className="fas fa-user-plus"></i>Sign up
-      </NavDropdown.Item>
+        <NavDropdown.Item 
+          id={styles.dropdownItem}
+          as={Link} 
+          to="/signin"
+          onClick={() => setToggleNavBar(!toggleNavBar)}
+        >
+          <i className="fas fa-sign-in-alt"></i>Sign in
+        </NavDropdown.Item>
+        <NavDropdown.Item 
+          id={styles.dropdownItem}
+          as={Link} 
+          to="/signup"
+          onClick={() => setToggleNavBar(!toggleNavBar)}
+        >
+          <i className="fas fa-user-plus"></i>Sign up
+        </NavDropdown.Item>
+      </NavDropdown>
     </>
   );
 
@@ -163,9 +137,7 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         <Navbar.Toggle 
-          onClick={() => {
-            setToggleNavBar(!toggleNavBar);
-          }}
+          onClick={() => setToggleNavBar(!toggleNavBar)}
           aria-controls="basic-navbar-nav" 
         />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -175,28 +147,16 @@ const NavBar = () => {
               className={styles.NavLink}
               activeClassName={styles.Active}
               to="/"
-              onClick={() => {
-                setToggleNavBar(!toggleNavBar);
-              }}
+              onClick={() => setToggleNavBar(!toggleNavBar)}
             >
               <i className="fas fa-home"></i>Home
             </NavLink>
             {currentUser ? loggedInIcons : loggedOutIcons}
-            <NavDropdown 
-              title={
-                <span>
-                    <i className="fas fa-user-alt ml-5"></i>
-                </span>
-              }
-              id="basic-nav-dropdown" 
-            >
-              {currentUser ? loggedInDropdownIcons : loggedOutDropdownIcons}
-            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
 export default NavBar;
