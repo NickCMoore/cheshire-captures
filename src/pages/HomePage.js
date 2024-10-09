f16d775054f5205448185207bd5216367bba943c
Nick Moore <moorenickc@protonmail.com>
2024-10-09 09:41:15 +0000

    feat(homepage): hide "Get Started" button for logged-in users

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useCurrentUser } from '../contexts/CurrentUserContext';
 import styles from '../styles/HomePage.module.css';
 
 const HomePage = () => {
+  const currentUser = useCurrentUser();
+
   return (
     <div className={styles.FullScreenBackground}>
       <div className={styles.FullScreenOverlay}></div>
       <div className={styles.FullScreenContent}>
         <h1>Welcome to Cheshire Captures</h1>
         <p>Capture and share your favourite moments with the community.</p>
-        <Link to="/signup">
-          <button className={styles.Button} aria-label="Sign up and get started">Get Started</button>
-        </Link>
+        {!currentUser && (
+          <Link to="/signup">
+            <button className={styles.Button} aria-label="Sign up and get started">
+              Get Started
+            </button>
+          </Link>
+        )}
       </div>
     </div>
   );
