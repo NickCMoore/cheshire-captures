ce50df09690cb31dd1a1987d03d6abf4374eb2e7
Nick Moore <moorenickc@protonmail.com>
2024-10-12 20:36:51 +0000

    fix: update retrieval of top photographers

--- src/pages/PopularPhotographers.js
+++ src/pages/PopularPhotographers.js
@@ -1,34 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+import { axiosReq } from '../api/axiosDefaults';
 import { useCurrentUser } from '../contexts/CurrentUserContext';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
-import SearchBar from '../components/SearchBar'; 
+import SearchBar from '../components/SearchBar';
 
 const PopularPhotographers = () => {
   const [photographers, setPhotographers] = useState([]);
+  const [filteredPhotographers, setFilteredPhotographers] = useState([]);
   const currentUser = useCurrentUser();
   const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     const fetchPhotographers = async () => {
       try {
-        const { data } = await axios.get(`/api/photos/photos/top-rated/`);
-        setPhotographers(data.results);
-      } catch (error) {
-        console.error('Error fetching photographers:', error);
+        const { data } = await axiosReq.get('/api/photographers/top-photographers/');
+        setPhotographers(data);
+        setFilteredPhotographers(data); 
+      } catch (err) {
+        console.error('Error fetching photographers:', err);
       }
     };
 
     fetchPhotographers();
-  }, [searchQuery]);
+  }, []);
+
+  useEffect(() => {
+    if (searchQuery) {
+      setFilteredPhotographers(
+        photographers.filter((photographer) =>
+          photographer.display_name.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      );
+    } else {
+      setFilteredPhotographers(photographers);
+    }
+  }, [searchQuery, photographers]);
 
   return (
     <Container>
       <h2 className="my-4">Popular Photographers</h2>
       <SearchBar onSearch={setSearchQuery} />
       <Row className="mt-4">
-        {photographers.map((photographer) => (
+        {filteredPhotographers.map((photographer) => (
           <Col key={photographer.id} md={4} className="mb-4">
             <Card className="shadow-sm">
               <Card.Body className="text-left">
@@ -39,7 +53,7 @@ const PopularPhotographers = () => {
                     View Profile
                   </Button>
                 </Link>
-                {currentUser?.username === photographer.user && (
+                {currentUser?.username === photographer.user.username && (
                   <Link to={`/profile/${photographer.id}/edit`}>
                     <Button variant="secondary" className="w-100 mt-2">
                       Edit Profile
