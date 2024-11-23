69fd193d15b57d9f1b2a7eebedf83424648cae28
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-11-23 09:39:40 +0000

    chore: update axios settings for env files

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,9 +1,16 @@
-
 import axios from "axios";
 
-axios.defaults.baseURL = "https://cheshire-captures-backend-084aac6d9023.herokuapp.com/";
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+// Set the base URL for all requests
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+
+// Set default headers
+axios.defaults.headers.post["Content-Type"] = "application/json"; // 
 axios.defaults.withCredentials = true;
 
+// Create Axios instances for requests and responses
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
+
+// Add CSRF token handling (required for Django with cookies)
+axios.defaults.xsrfCookieName = "csrftoken"; 
+axios.defaults.xsrfHeaderName = "X-CSRFToken"; 
