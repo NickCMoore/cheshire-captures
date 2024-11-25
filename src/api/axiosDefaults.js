354bdb0be81c23e279e54708dfa56203c1ecf750
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-11-25 14:32:46 +0000

    chore: update axios headers for file upload formats

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,16 +1,10 @@
 import axios from "axios";
 
 // Set the base URL for all requests
-axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+axios.defaults.baseURL = "https://cheshire-captures-4a500dc7ab0a.herokuapp.com/"
 
-// Set default headers
-axios.defaults.headers.post["Content-Type"] = "application/json"; // 
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data"; 
 axios.defaults.withCredentials = true;
 
-// Create Axios instances for requests and responses
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
-
-// Add CSRF token handling (required for Django with cookies)
-axios.defaults.xsrfCookieName = "csrftoken"; 
-axios.defaults.xsrfHeaderName = "X-CSRFToken"; 
